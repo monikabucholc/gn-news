709987d989ab3dc0789d087acd86477fd2752a1e
monikabucholc <mnkbucholc@gmail.com>
2023-03-29 10:32:55 +0200

    Fix

--- src/utils/newsapi.js
+++ src/utils/newsapi.js
@@ -1,11 +1,12 @@
 import { availableInNewsApi } from '../data/countriesList'
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+const URL = process.env.REACT_APP_URL
 
     
 export const fetchNews = async (request) => {
     const currentCountry = request.toLowerCase();
     if (availableInNewsApi.includes(currentCountry)) {
-    const urlCountry = 'https://newsapi.org/v2/top-headlines?' +
+    const urlCountry = `${URL}` +
                         `country=${currentCountry}&` +
                         `apiKey=${API_KEY}`;
     const req = new Request(urlCountry);
